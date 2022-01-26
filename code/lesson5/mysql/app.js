2fa02d71ec769278cb9bf808159a4a162d8b8631
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2022-01-26 20:48:46 +0100

    Very basic INSERT example

---
+++ code/lesson5/mysql/app.js
@@ -0,0 +1,43 @@
+const mysql = require("mysql");
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hcs",
+  connectionLimit: 10,
+});
+
+const express = require("express");
+const path = require("path");
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json()); // !important to parse json POST requests
+
+// insert something into the database
+// we use post to insert new rows to the database (create new rows)
+app.post("/database", (req, res) => {
+  // use the pool, so that we don't have blocking data
+  // req.body is the json data, we sent from the browser (client)
+  // with this format we can just pass the json data and the mysql package is
+  // correctly assigning it to the table columns
+  pool.query(
+    "INSERT INTO mytable SET ?",
+    req.body,
+    (error, results, fields) => {
+      // did we have a mysql error?
+      if (error) {
+        // yes: returning the error
+        res.statusCode(500).send({ ok: false, error: error });
+      } else {
+        // no: return a confirmation
+        res.send({ ok: true });
+      }
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
