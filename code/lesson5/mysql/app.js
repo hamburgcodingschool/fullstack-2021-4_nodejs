4e9f732f2ea61ad113843608b7ea70fbbf739765
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2022-01-26 20:54:52 +0100

    Added more comments

--- code/lesson5/mysql/app.js
+++ code/lesson5/mysql/app.js
@@ -1,4 +1,7 @@
+// use the mysql package
 const mysql = require("mysql");
+// create a connection pool
+// with the pool we can re-use connections more easily
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
@@ -7,17 +10,19 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
+// standard way to get the express server
 const express = require("express");
 const path = require("path");
 const PORT = 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); // serve static content from public directory
 app.use(express.json()); // !important to parse json POST requests
 
 // insert something into the database
 // we use post to insert new rows to the database (create new rows)
 app.post("/database", (req, res) => {
+    console.log(req.body);
   // use the pool, so that we don't have blocking data
   // req.body is the json data, we sent from the browser (client)
   // with this format we can just pass the json data and the mysql package is
@@ -31,6 +36,7 @@ app.post("/database", (req, res) => {
         // yes: returning the error
         res.statusCode(500).send({ ok: false, error: error });
       } else {
+          console.log("data inserted into database");
         // no: return a confirmation
         res.send({ ok: true });
       }
