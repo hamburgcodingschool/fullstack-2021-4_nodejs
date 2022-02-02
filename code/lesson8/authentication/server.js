1450cfd1ce932834e18ccb14b4a45667457b686f
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2022-02-02 20:21:47 +0100

    Setting username and password via .env

--- code/lesson8/authentication/server.js
+++ code/lesson8/authentication/server.js
@@ -1,55 +1,65 @@
 // there is no convention in naming it app.js
 
+// read the .env file
+require("dotenv").config();
+
+// If you set variables in the terminal:
+// `export APP_USERNAME=foo`
+// and to delete:
+// `unset APP_USERNAME`
+
 const express = require("express");
 const PORT = 3000;
 const app = express();
 
 app.get("/admin", (req, res) => {
-    // if the user has entered username and password, it is in here
-    const header = req.headers.authorization;
-    let is_user_authenticated;
-    // is it in there?
-    if (header) {
-        // The header reads like "Basic asdkahsdlkj==" 
-        // and we are only interested in the second part
-        const b64auth = header.split(" ")[1];
-        // We need to decode the string
-        const auth = Buffer.from(b64auth, "base64").toString()
-        // Now auth reads like "username:password"
+  // if the user has entered username and password, it is in here
+  const header = req.headers.authorization;
+  let is_user_authenticated;
+  // is it in there?
+  if (header) {
+    // The header reads like "Basic asdkahsdlkj=="
+    // and we are only interested in the second part
+    const b64auth = header.split(" ")[1];
+    // We need to decode the string
+    const auth = Buffer.from(b64auth, "base64").toString();
+    // Now auth reads like "username:password"
 
-        // split converts the buffer string into an array
-        const [username, password] = auth.split(":");
+    // split converts the buffer string into an array
+    const [username, password] = auth.split(":");
 
-
-        // check, if username and password are correct
-        // NORMALLY:
-        // - hash the password (maybe with a salt)
-        // - compare username and hash against a database
-        // hashing: convert text to numbers, but different text converts to different numbers
-        // known hashing mechanisms: MD5 (unsafe!), SHA, SHA256, SHA512
-        // salt: Add some (fixed) string to the password before hasing it, so that if you get the hash
-        //     that you can't guess the password from it
-        //
-        // NEVER:
-        // Compare against fixed strings in your code
-        if (username === "foo" && password === "bar") {
-            is_user_authenticated = true;
-        } else {
-            is_user_authenticated = false;
-        }
+    // check, if username and password are correct
+    // NORMALLY:
+    // - hash the password (maybe with a salt)
+    // - compare username and hash against a database
+    // hashing: convert text to numbers, but different text converts to different numbers
+    // known hashing mechanisms: MD5 (unsafe!), SHA, SHA256, SHA512
+    // salt: Add some (fixed) string to the password before hasing it, so that if you get the hash
+    //     that you can't guess the password from it
+    //
+    // NEVER:
+    // Compare against fixed strings in your code
+    if (
+      username === process.env.APP_USERNAME &&
+      password === process.env.APP_PASSWORD
+    ) {
+      is_user_authenticated = true;
     } else {
-        is_user_authenticated = false;
+      is_user_authenticated = false;
     }
+  } else {
+    is_user_authenticated = false;
+  }
 
-    if (is_user_authenticated) {
-        res.send("Very secret admin content");
-    } else {
-        // tell the browser, that some authentication is reuqired
-        // AND tell, what kind of authentication is expected
-        res.set("WWW-Authenticate", 'Basic realm="401"'); // set http header in the response
-        res.status(401).send("Authentication required.");
-        // send() always needs to be the last command
-    }
+  if (is_user_authenticated) {
+    res.send("Very secret admin content");
+  } else {
+    // tell the browser, that some authentication is reuqired
+    // AND tell, what kind of authentication is expected
+    res.set("WWW-Authenticate", 'Basic realm="401"'); // set http header in the response
+    res.status(401).send("Authentication required.");
+    // send() always needs to be the last command
+  }
 });
 
 app.get("/about", (req, res) => {
@@ -61,5 +71,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`);
+  console.log(
+    `Server started on ${PORT} - our admin user is ${process.env.APP_USERNAME}`
+  );
 });
