7af2aaaa46519c4a0079d925cf7b4e5efc8a0917
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2022-02-02 19:39:27 +0100

    Sending authentication request

--- code/lesson8/authentication/server.js
+++ code/lesson8/authentication/server.js
@@ -5,7 +5,11 @@ const PORT = 3000;
 const app = express();
 
 app.get("/admin", (req, res) => {
-  res.send("not everyone should change this");
+    // tell the browser, that some authentication is reuqired
+    // AND tell, what kind of authentication is expected
+    res.set("WWW-Authenticate", 'Basic realm="401"'); // set http header in the response
+    res.status(401).send("Authentication required.");
+    // send() always needs to be the last command
 });
 
 app.get("/about", (req, res) => {
